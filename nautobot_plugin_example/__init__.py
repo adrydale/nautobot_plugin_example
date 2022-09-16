c58b0b3d8973896629367c761f6a3f12874fb46e
Aaron Drydale <aadrjm@gmail.com>
2022-09-16 13:29:22 -0500

    Name changes again

--- nautobot_plugin_example/__init__.py
+++ nautobot_plugin_example/__init__.py
@@ -3,12 +3,12 @@
 from nautobot.extras.plugins import PluginConfig
 
 
-class NautobotExamplePluginConfig(PluginConfig):
+class NautobotPluginExampleConfig(PluginConfig):
     """Plugin configuration for the nautobot_example_plugin plugin."""
 
-    name = "nautobot_example_plugin"  # Raw plugin name; same as the plugin's source directory.
-    verbose_name = "nautobot_example_plugin"  # Human-friendly name for the plugin.
-    base_url = "nautobot_example_plugin"  # (Optional) Base path to use for plugin URLs. Defaulting to app_name.
+    name = "nautobot_plugin_example"  # Raw plugin name; same as the plugin's source directory.
+    verbose_name = "nautobot_plugin_example"  # Human-friendly name for the plugin.
+    base_url = "nautobot_plugin_example"  # (Optional) Base path to use for plugin URLs. Defaulting to app_name.
     required_settings = []  # A list of any configuration parameters that must be defined by the user.
     min_version = "1.0.0"  # Minimum version of Nautobot with which the plugin is compatible.
     max_version = "1.999"  # Maximum version of Nautobot with which the plugin is compatible.
@@ -16,4 +16,4 @@ class NautobotExamplePluginConfig(PluginConfig):
     caching_config = {}  # Plugin-specific cache configuration.
 
 
-config = NautobotExamplePluginConfig
+config = NautobotPlugiinExampleConfig
